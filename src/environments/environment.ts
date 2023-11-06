5fbd7c82282390c1b1f39cee510ff80752e5c4d3
manoj <manojthiwari890@gmail.com>
2023-11-06 18:07:16 +0530

    added / in environment | 1.0.5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     version: require("../../package.json").version,
-    api:"https://e-commerce-api-scoe.onrender.com"
+    api:"https://e-commerce-api-scoe.onrender.com/"
 };
