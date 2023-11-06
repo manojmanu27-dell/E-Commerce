a5ef926094b7e6c16cbfba213e299a417cdeb21b
manoj <manojthiwari890@gmail.com>
2023-11-06 18:21:40 +0530

    testing with workflows | 1.0.6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ApiCallsService } from './services/api-calls/api-calls.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ApiCallsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
