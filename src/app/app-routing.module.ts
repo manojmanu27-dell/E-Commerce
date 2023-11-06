02577b7ab2ac4b39d49e65e71653eb29ac682141
manoj <manojthiwari890@gmail.com>
2023-11-06 10:36:00 +0530

    added location strategy | 1.0.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{provide:LocationStrategy,useClass:HashLocationStrategy}]
 })
 export class AppRoutingModule { }
