5a3579092791a43950a8eb609cb595b96046151f
manoj <manojthiwari890@gmail.com>
2023-11-06 10:25:34 +0530

    initial commit | 1.0.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(){
+    console.log("The Ui is On version",environment.version ," and the production build is :-",environment.production)
+  }
+  title = 'E-Commerce-UI';
+}
